679887239b20e79cad14c46e151e8c87bc03b3a2
ilabstaff <ilabstaff@172.16.116.81>
2019-03-19 17:50:53 -0700

    finished slot machine, going to work on whack-a-mole next class.

--- src/extra/moneytrashbin.java
+++ src/extra/moneytrashbin.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -16,7 +17,10 @@ public class moneytrashbin implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
-	JButton button =new JButton();
+	JButton button = new JButton();
+	int max = 3;
+
+	Random ran = new Random();
 
 	public static void main(String[] args) {
 		moneytrashbin mtb = new moneytrashbin();
@@ -27,31 +31,73 @@ public class moneytrashbin implements ActionListener {
 		frame.add(panel);
 		panel.add(button);
 		frame.setVisible(true);
-		//frame.setSize(500, 500);
+		frame.setSize(750, 750);
 		button.setText("SPIN");
 		button.addActionListener(this);
-		panel.add(createLabelImage("cherry.jpg"));
-		panel.add(createLabelImage("orange.jpg"));
-		panel.add(createLabelImage("xbox.png"));
-		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
+
 	}
-	private JLabel createLabelImage(String fileName){
-        URL imageURL = getClass().getResource(fileName);
-	if (imageURL == null){
-		System.err.println("Could not find image " + fileName);
-		return new JLabel();
+
+	private JLabel createLabelImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
 	}
-	Icon icon = new ImageIcon(imageURL);
-	JLabel imageLabel = new JLabel(icon);
-	return imageLabel;
-}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		panel.removeAll();
+		panel.add(button);
+		int randy = ran.nextInt(max);
+		int randy2 = ran.nextInt(max);
+		int randy3 = ran.nextInt(max);
+		if (randy == 0) {
+			JLabel labelbean = createLabelImage("Bean.jpg");
+			panel.add(labelbean);
+
+		} else if (randy == 1) {
+			JLabel labelelfo = createLabelImage("elfo.jpg");
+			panel.add(labelelfo);
+		} else if (randy == 2) {
+			JLabel labelluci = createLabelImage("luci.jpg");
+			panel.add(labelluci);
+
+		}
+		if (randy2 == 0) {
+			JLabel labelbean = createLabelImage("Bean.jpg");
+			panel.add(labelbean);
+
+		} else if (randy2 == 1) {
+			JLabel labelelfo = createLabelImage("elfo.jpg");
+			panel.add(labelelfo);
+		} else if (randy2 == 2) {
+			JLabel labelluci = createLabelImage("luci.jpg");
+			panel.add(labelluci);
+		}
+		if (randy3 == 0) {
+			JLabel labelbean = createLabelImage("Bean.jpg");
+			panel.add(labelbean);
+
+		} else if (randy3 == 1) {
+			JLabel labelelfo = createLabelImage("elfo.jpg");
+			panel.add(labelelfo);
+		} else if (randy3 == 2) {
+			JLabel labelluci = createLabelImage("luci.jpg");
+			panel.add(labelluci);
+		}
 		
+		
+		if (randy==randy2&&randy==randy3) {
+			label.setText("YOU WIN!!!!!!!!!!!!");
+			panel.add(label);
+		}
+	frame.pack();
 	}
-    
+
 }
